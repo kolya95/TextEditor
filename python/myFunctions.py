070b14733eb467c55add622cbcff46a55056f544
kolya <kolya@linux-cllz.site>
2013-11-29 07:15:49 +0400

    py files

---
+++ python/myFunctions.py
@@ -0,0 +1,24 @@
+import io
+import sys
+
+
+class MyIO(io.TextIOBase):
+    def __init__(self):
+        super().__init__()
+        try:
+            import _myModule
+
+            self.write = _myModule.myPrint
+        except ImportError:
+            self.write = sys.__stdout__.write
+
+
+sys.stdout = MyIO()
+
+
+def func(s):
+    a = compile(s, "", "exec")
+    return exec(a)
+
+if __name__ == "__main__":
+    pass
