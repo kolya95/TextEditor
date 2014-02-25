56178dd458ddadade42759344e339233f49941f8
kolya95 <nikolaypopov95@gmail.com>
2014-02-25 23:20:10 +0400

    number line deleted fixed bug with shift+tab sequency

--- mytextedit.cpp
+++ mytextedit.cpp
@@ -4,41 +4,86 @@ MyTextEdit::MyTextEdit(QWidget *parent) :
     QTextEdit(parent)
 {
     QTextEdit::setWordWrapMode(QTextOption::NoWrap);
-
-    //*********************
-
-
-    lineNumberArea = new LineNumberArea(this);
-
-    connect(this, SIGNAL(blockCountChanged(int)), this, SLOT(updateLineNumberAreaWidth(int)));
-    connect(this, SIGNAL(updateRequest(QRect,int)), this, SLOT(updateLineNumberArea(QRect,int)));
-    connect(this, SIGNAL(cursorPositionChanged()), this, SLOT(highlightCurrentLine()));
-
-
-
-    QAction* myShortcut = new QAction(this);
-    myShortcut->setShortcut(QKeySequence(Qt::SHIFT + Qt::Key_Tab));
-
-    connect(myShortcut, SIGNAL(triggered()), this, SLOT(ShitTabSequency()));
-    addAction(myShortcut);
-
-    updateLineNumberAreaWidth(0);
-    highlightCurrentLine();
-
 }
 void MyTextEdit::keyPressEvent(QKeyEvent *e)
 {
-    if(e->key()==Qt::Key_Tab||e->key()==Qt::Key_Backspace)
+    if(e->key()==Qt::Key_Tab||e->key()==Qt::Key_Backspace||e->key()==Qt::Key_Backtab||(e->key() == Qt::Key_Tab && e->modifiers() & Qt::ShiftModifier) )
     {
+        if(e->key() == Qt::Key_Backtab||(e->key() == Qt::Key_Tab && e->modifiers() & Qt::ShiftModifier))
+        {
+            qDebug()<<"SHIFT+TAB";
+            QString txt = toPlainText();
+            QTextCursor* cur = new QTextCursor(this->textCursor());
+            QTextCursor* c = new QTextCursor(*cur);
+            c->setPosition(cur->selectionStart());
+            qDebug()<<c->position();
+            int index = cur->selectionStart();
+            int count = 0;
+            while(index>0&&txt[index]!=QChar::ParagraphSeparator&&txt[index]!='\n')
+            {
+                index--;
+                count++;
+            }
+            c->setPosition(index);
+            qDebug()<<c->position();
+            c->movePosition(QTextCursor::Right,QTextCursor::KeepAnchor, count+cur->selectedText().size());
+            count+=cur->selectedText().size();
+            int count2 = 0;
 
+            while(c->selectionEnd()<txt.size()&&txt[c->selectionEnd()]!=QChar::ParagraphSeparator&&txt[c->selectionEnd()]!='\n')
+            {
+                count++;
+                if(!c->movePosition(QTextCursor::Right,QTextCursor::KeepAnchor,1))
+                {
+                    break;
+                }
+            }
+            QString t = c->selectedText();
+            qDebug()<<t;
+            if(t[0]!=QChar::ParagraphSeparator&&t[0]!='\n')
+            {
+                for(int i = 0; i<4; i++)
+                {
+                    if(t[0]!=' ')
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        count2--;
+                        t.remove(0,1);
+                    }
+                }
+            }
+            for(int i = 0; i < t.size(); i++)
+            {
+                if(t[i]=='\n'||t[i]==QChar::ParagraphSeparator)
+                {
+                    for(int j = 0; j<4; j++)
+                    {
+                        if(t[i+1]==' ')
+                        {
+                            t.remove(i+1,1);
+                           count2--;
+                        }
+                        else break;
+                    }
+                }
+            }
 
+            c->insertText(t);
+            cur->setPosition(index/*+1*/);
+            qDebug()<<cur->selectionStart()<<cur->selectionEnd();
+            cur->movePosition(QTextCursor::Right,QTextCursor::KeepAnchor,count2+count);
+            this->setTextCursor(*cur);
+        }
     }
     else
         QTextEdit::keyPressEvent(e);
 }
-
 void MyTextEdit::keyReleaseEvent(QKeyEvent *e)
 {
+
     if(e->key()==Qt::Key_Return||e->key()==Qt::Key_Enter||e->key()==Qt::Key_Backspace||e->key()==Qt::Key_Tab)
     {
         QTextCursor *cur = new QTextCursor;
@@ -175,170 +220,3 @@ void MyTextEdit::keyReleaseEvent(QKeyEvent *e)
     }
     setCursorWidth(1);
 }
-void MyTextEdit::ShiftTabSequency()
-{
-    qDebug()<<"SHIFT+TAB";
-    QString txt = toPlainText();
-    QTextCursor* cur = new QTextCursor(this->textCursor());
-    QTextCursor* c = new QTextCursor(*cur);
-    c->setPosition(cur->selectionStart());
-    qDebug()<<c->position();
-    int index = cur->selectionStart();
-    int count = 0;
-    while(index>0&&txt[index]!=QChar::ParagraphSeparator&&txt[index]!='\n')
-    {
-        index--;
-        count++;
-    }
-    c->setPosition(index);
-    qDebug()<<c->position();
-    c->movePosition(QTextCursor::Right,QTextCursor::KeepAnchor, count+cur->selectedText().size());
-    count+=cur->selectedText().size();
-    int count2 = 0;
-
-    while(c->selectionEnd()<txt.size()&&txt[c->selectionEnd()]!=QChar::ParagraphSeparator&&txt[c->selectionEnd()]!='\n')
-    {
-        count++;
-        if(!c->movePosition(QTextCursor::Right,QTextCursor::KeepAnchor,1))
-        {
-            break;
-        }
-    }
-    QString t = c->selectedText();
-    qDebug()<<t;
-    if(t[0]!=QChar::ParagraphSeparator&&t[0]!='\n')
-    {
-        for(int i = 0; i<4; i++)
-        {
-            if(t[0]!=' ')
-            {
-                break;
-            }
-            else
-            {
-                count2--;
-                t.remove(0,1);
-            }
-        }
-    }
-    for(int i = 0; i < t.size(); i++)
-    {
-        if(t[i]=='\n'||t[i]==QChar::ParagraphSeparator)
-        {
-            for(int j = 0; j<4; j++)
-            {
-                if(t[i+1]==' ')
-                {
-                    t.remove(i+1,1);
-                   count2--;
-                }
-                else break;
-            }
-        }
-    }
-
-    c->insertText(t);
-    cur->setPosition(index+1);
-    qDebug()<<cur->selectionStart()<<cur->selectionEnd();
-    cur->movePosition(QTextCursor::Right,QTextCursor::KeepAnchor,count2+count);
-    this->setTextCursor(*cur);
-
-}
-
-//*************
-
-
- int MyTextEdit::lineNumberAreaWidth()
- {
-     int digits = 1;
-     int max = qMax(1, this->document()->blockCount());
-     while (max >= 10) {
-         max /= 10;
-         ++digits;
-     }
-
-     int space = 3 + fontMetrics().width(QLatin1Char('9')) * digits;
-
-     return space;
- }
-
-
-
- void MyTextEdit::updateLineNumberAreaWidth(int /* newBlockCount */)
- {
-     setViewportMargins(lineNumberAreaWidth(), 0, 0, 0);
- }
-
-
-
- void MyTextEdit::updateLineNumberArea(const QRect &rect, int dy)
- {
-     if (dy)
-         lineNumberArea->scroll(0, dy);
-     else
-         lineNumberArea->update(0, rect.y(), lineNumberArea->width(), rect.height());
-
-     if (rect.contains(viewport()->rect()))
-         updateLineNumberAreaWidth(0);
- }
-
-
-
- void MyTextEdit::resizeEvent(QResizeEvent *e)
- {
-     QTextEdit::resizeEvent(e);
-
-     QRect cr = contentsRect();
-     lineNumberArea->setGeometry(QRect(cr.left(), cr.top(), lineNumberAreaWidth(), cr.height()));
-}
-
-
-
- void MyTextEdit::highlightCurrentLine()
- {
-     QList<QTextEdit::ExtraSelection> extraSelections;
-
-     if (!isReadOnly()) {
-         QTextEdit::ExtraSelection selection;
-
-         QColor lineColor = QColor(Qt::yellow).lighter(160);
-
-         selection.format.setBackground(lineColor);
-         selection.format.setProperty(QTextFormat::FullWidthSelection, true);
-         selection.cursor = textCursor();
-         selection.cursor.clearSelection();
-         extraSelections.append(selection);
-     }
-
-     setExtraSelections(extraSelections);
- }
-
-
-
- void MyTextEdit::lineNumberAreaPaintEvent(QPaintEvent *event)
- {
-     QPainter painter(lineNumberArea);
-     painter.fillRect(event->rect(), Qt::lightGray);
-
-
-     QTextBlock block = this->document()->firstBlock();
-     int blockNumber = block.blockNumber();
-
-     int top = (int) 5;
-     int bottom = top + (int) 16;
-
-     while (block.isValid() && top <= event->rect().bottom()) {
-         if (bottom >= event->rect().top())
-         {
-             QString number = QString::number(blockNumber + 1);
-             painter.setPen(Qt::black);
-             painter.drawText(0, top, lineNumberArea->width(), fontMetrics().height(),
-                              Qt::AlignRight, number);
-         }
-
-         block = block.next();
-         top = bottom;
-         bottom = top + (int) 16;
-         ++blockNumber;
-     }
- }
--- mytextedit.h
+++ mytextedit.h
@@ -10,46 +10,8 @@ public:
 
     void keyReleaseEvent(QKeyEvent *e);
     void keyPressEvent(QKeyEvent *e);
-//**********************************
-    void lineNumberAreaPaintEvent(QPaintEvent *event);
-    int lineNumberAreaWidth();
-
-protected:
-    void resizeEvent(QResizeEvent *event);
-
-public Q_SLOTS:
-    void updateLineNumberAreaWidth(int newBlockCount);
-    void highlightCurrentLine();
-    void updateLineNumberArea(const QRect &, int);
-
-
-    void ShiftTabSequency();
-
-
-private:
-
-    QWidget *lineNumberArea;
 };
 
 
 
- class LineNumberArea : public QWidget
- {
- public:
-     LineNumberArea(MyTextEdit *editor) : QWidget(editor) {
-         codeEditor = editor;
-     }
-
-     QSize sizeHint() const {
-         return QSize(codeEditor->lineNumberAreaWidth(), 0);
-     }
-
- protected:
-     void paintEvent(QPaintEvent *event) {
-         codeEditor->lineNumberAreaPaintEvent(event);
-     }
-
- private:
-     MyTextEdit *codeEditor;
- };
 #endif // MYTEXTEDIT_H
